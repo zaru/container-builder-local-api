b6d8a3c2879efe9ca720eb5f82156b7600f09706
zaru <sakuraba@basicinc.jp>
2017-10-03 17:11:46 +0900

    Added dummy response

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"net/http"
-
+	"fmt"
+	"io/ioutil"
+	"os"
+	"encoding/json"
 	"github.com/labstack/echo"
 )
 
@@ -12,21 +15,43 @@ func main() {
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-type User struct {
-	Steps string `json:"steps[]"`
-}
+//MEMO: これは使わない…かな…中身をパースすることはなさそう
 type BuildParams struct {
-	Name       string   `json:"name"`
-	Env        []string `json:"env"`
-	Args       []string `json:"args"`
-	WaitFor    []string `json:"waitFor"`
-	Entrypoint string   `json:"entrypoint"`
+	Steps []struct {
+		Name       string   `json:"name"`
+		Env        []string `json:"env"`
+		Args       []string `json:"args"`
+		WaitFor    []string `json:"waitFor"`
+		Entrypoint string   `json:"entrypoint"`
+	} `json:"steps"`
+	LogsBucket string `json:"logsBucket"`
+}
+
+type Response struct {
+	Metadata struct {
+		Build struct {
+			ID string `json:"id"`
+		} `json:"build"`
+	} `json:"metadata"`
 }
 
 func save(c echo.Context) error {
-	post := new(BuildParams)
-	if err := c.Bind(post); err != nil {
-		return err
+	body, _ := ioutil.ReadAll(c.Request().Body)
+	content := []byte(string(body))
+	//TODO: cloudbuild.yamlファイルをワーキングディレクトリに出力する
+	ioutil.WriteFile("/tmp/go-file", content, os.ModePerm)
+	fmt.Print(string(body))
+
+	//TODO: 動的にユニークなビルドIDを適当に返す
+	build_id := "build-1234"
+	jsonResponse := `{"metadata": {"build": {"id": "` + build_id + `"}}}`
+	res := &Response{}
+	err := json.Unmarshal([]byte(jsonResponse), res)
+	if(err!=nil) {
+		return echo.NewHTTPError(http.StatusUnauthorized, "Please provide valid credentials")
 	}
-	return c.JSON(http.StatusCreated, post)
+
+	//TODO: container-builder-localでビルドする
+
+	return c.JSON(http.StatusCreated, res)
 }
